a35c0f640d883dd6625b33d1aa4c16eaeae7dd93
yujin <kingyujin56@gmail.com>
2025-04-14 15:18:42 +0900

    #2.3 'As' and Attrs

--- src/App.js
+++ src/App.js
@@ -4,21 +4,18 @@ const Father = styled.div`
   display: flex;
 `;
 
-const Box = styled.div`
-  background-color: ${(props) => props.bgColor};
-  width: 100px;
-  height: 100px;
-`;
-
-const Circle = styled(Box)`
-  border-radius: 50px;
+const Input = styled.input.attrs((props) => ({
+  required: props.rq,
+  placeholder: props.ph || "text",
+}))`
+  background-color: tomato;
 `;
 
 function App() {
   return (
     <Father>
-      <Box bgColor="teal" />
-      <Circle bgColor="whitesmoke" />
+      <Input rq={true} />
+      <Input rq={false} ph={"nickname"} />
     </Father>
   );
 }
