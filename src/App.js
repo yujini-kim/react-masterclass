7f5362bec1f712aa5c2f3c7bc6c1f2cc46508c46
yujin <kingyujin56@gmail.com>
2025-04-14 16:12:29 +0900

    #2.7 Themes

--- src/App.js
+++ src/App.js
@@ -6,45 +6,17 @@ const Wrapper = styled.div`
   width: 100vw;
   justify-content: center;
   align-items: center;
+  background-color: ${(props) => props.theme.bgColor};
 `;
 
-const rotationAnimation = keyframes`
-0%{
-transform:rotate(0deg);
-border-radius:0px
-} 50% {
-  transform:rotate(360deg);
-  border-radius:50px
-} 100% {
-  transform:rotate(0deg);
-border-radius:0px
-}
-`;
-
-const Emoji = styled.span`
-  font-size: 36px;
-`;
-
-const Box = styled.div`
-  width: 100px;
-  height: 100px;
-  background-color: tomato;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  animation: ${rotationAnimation} 2s linear infinite;
-  ${Emoji}:hover {
-    font-size: 90px;
-  }
+const Title = styled.h1`
+  color: ${(props) => props.theme.textColor};
 `;
 
 function App() {
   return (
     <Wrapper>
-      <Box>
-        <Emoji>😆</Emoji>
-      </Box>
-      <Emoji>😆</Emoji>
+      <Title>Hello</Title>
     </Wrapper>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { ThemeProvider } from "styled-components";
+
+const darkTheme = {
+  textColor: "whitesmoke",
+  bgColor: "#111",
+};
+
+const lightTheme = {
+  textColor: "#111",
+  bgColor: "whitesmoke",
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={darkTheme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
