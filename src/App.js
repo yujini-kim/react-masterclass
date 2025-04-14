fb6688d720b7050ccffa0a207faa103dde4a71f6
yujin <kingyujin56@gmail.com>
2025-04-14 15:36:37 +0900

    #2.4 Animations and Pseudo Selectors

--- src/App.js
+++ src/App.js
@@ -1,22 +1,48 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
-const Father = styled.div`
+const Wrapper = styled.div`
   display: flex;
 `;
 
-const Input = styled.input.attrs((props) => ({
-  required: props.rq,
-  placeholder: props.ph || "text",
-}))`
+const rotationAnimation = keyframes`
+0%{
+transform:rotate(0deg);
+border-radius:0px
+} 50% {
+  transform:rotate(360deg);
+  border-radius:50px
+} 100% {
+  transform:rotate(0deg);
+border-radius:0px
+}
+`;
+
+const Box = styled.div`
+  width: 100px;
+  height: 100px;
   background-color: tomato;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  animation: ${rotationAnimation} 2s linear infinite;
+  span {
+    font-size: 36px;
+    &:hover {
+      font-size: 50px;
+    }
+    &:active {
+      opacity: 0;
+    }
+  }
 `;
 
 function App() {
   return (
-    <Father>
-      <Input rq={true} />
-      <Input rq={false} ph={"nickname"} />
-    </Father>
+    <Wrapper>
+      <Box>
+        <span>😆</span>
+      </Box>
+    </Wrapper>
   );
 }
 
