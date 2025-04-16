ad0a6219ce4fc14f5e7e28afc3eecdf71f102912
yujin <kingyujin56@gmail.com>
2025-04-16 13:46:25 +0900

    #4.1 BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,11 @@
-import { useState } from "react";
 import styled from "styled-components";
-
-const Container = styled.div`
-  background-color: ${(props) => props.theme.bgColor};
-`;
-const H1 = styled.h1`
-  color: ${(props) => props.theme.textColor};
-`;
+import Router from "./Router";
 
 function App() {
   return (
-    <Container>
-      <H1>protected</H1>
-    </Container>
+    <div>
+      <Router />
+    </div>
   );
 }
 
