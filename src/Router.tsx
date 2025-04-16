b0e72abbca861fe01890fea0e06bc410b8072dca
yujin <kingyujin56@gmail.com>
2025-04-16 14:09:38 +0900

    #4.4 useNavigate

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import Root from "./Root";
 import About from "./screens/About";
 import Home from "./screens/Home";
+import NotFound from "./screens/NotFound";
 
 const router = createBrowserRouter([
   {
@@ -11,6 +12,7 @@ const router = createBrowserRouter([
       { path: "", element: <Home /> },
       { path: "about", element: <About /> },
     ],
+    errorElement: <NotFound />,
   },
 ]);
 
---
+++ src/screens/NotFound.tsx
@@ -0,0 +1,5 @@
+function NotFound() {
+  return <>Err0r</>;
+}
+
+export default NotFound;
