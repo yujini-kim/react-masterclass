1a7f4ad02c6aed6fcf612cd62c8692a9525ae152
yujin <kingyujin56@gmail.com>
2025-04-16 14:02:05 +0900

    #4.2 createBrowserRoute

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,18 +1,17 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from "./component/Header";
+import { createBrowserRouter } from "react-router-dom";
+import Root from "./Root";
 import About from "./screens/About";
 import Home from "./screens/Home";
 
-function Router() {
-  return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/about" element={<About />}></Route>
-      </Routes>
-    </BrowserRouter>
-  );
-}
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    children: [
+      { path: "", element: <Home /> },
+      { path: "about", element: <About /> },
+    ],
+  },
+]);
 
-export default Router;
+export default router;
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import { ThemeProvider } from "styled-components";
-import { darkTheme, lightTheme } from "./theme";
+import { RouterProvider } from "react-router-dom";
+import router from "./Router";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(
-  <React.StrictMode>
-    <ThemeProvider theme={darkTheme}>
-      <App />
-    </ThemeProvider>
-  </React.StrictMode>
-);
+
+root.render(<RouterProvider router={router} />);
